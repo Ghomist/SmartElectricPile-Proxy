bc3579d06732df6e2ba2f4a23ad1405942b9b2a9
Ghomist <ghomist@gmail.com>
2022-03-25 23:21:52 +0800

    add: try-except

--- main.py
+++ main.py
@@ -13,24 +13,29 @@ def main():
     # huawei cloud
     cloud.init(cfg['device_id'], cfg['secret'])
     cloud_client = cloud.get_cloud_client()
-    cloud_client.connect(cfg['cloud']['url'], port=cfg['cloud']['port'])
-    cloud_client.subscribe((cfg['cloud']['topics']['cmd-down'], 0))
+    try:
+        cloud_client.connect(cfg['cloud']['url'], port=cfg['cloud']['port'])
+        cloud_client.subscribe((cfg['cloud']['topics']['cmd-down'], 0))
+    except ConnectionError:
+        print("Failed connecting to cloud.")
+        exit()
 
     # raspberry pie
     pie.init()
     local_client = pie.get_local_client()
-    local_client.connect(cfg['local']['url'], port=cfg['local']['port'])
-    local_client.subscribe(("dev/+/up/data", 0))
+    try:
+        local_client.connect(cfg['local']['url'], port=cfg['local']['port'])
+        local_client.subscribe(("dev/+/up/data", 0))
+    except ConnectionRefusedError:
+        print("No local broker found. Try with 'mosquitto'.")
+        exit()
 
     # Start
-    try:
-        Thread(None, cloud_client.loop_forever).start()
-        Thread(None, local_client.loop_forever).start()
-        # print("Connecting finished.")
-    except KeyboardInterrupt:
-        cloud_client.disconnect()
-        local_client.disconnect()
-        print("User keyboard interrupt.")
+    Thread(None, cloud_client.loop_forever).start()
+    Thread(None, local_client.loop_forever).start()
+    # print("Connecting finished.")
+    # cloud_client.disconnect()
+    # local_client.disconnect()
 
 
 if __name__ == "__main__":
