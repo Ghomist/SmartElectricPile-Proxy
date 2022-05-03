591170bec21444e85c7f635bd13aecfd53bf24a3
Ghomist <ghomist@gmail.com>
2022-05-03 13:13:42 +0800

    update: cfg

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ def _init():
     config = {}
     # Init config
     with open('DEVICES-KEY.bib') as f:
-        config.update(json.loads(f.read()))
+        config.update(json.load(f))
     with open('config.json') as f:
-        config.update(json.loads(f.read()))
+        config.update(json.load(f))
         # Update device id into config
         for k, topic in config['cloud']['topics'].items():
             config['cloud']['topics'][k] = topic.format(config['device_id'])
